7feb036011fe432db28291a7f8fc5ca06542400a
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-01-09 11:26:30 +0530

    Update DEVIL.py

--- userbot/plugins/DEVIL.py
+++ userbot/plugins/DEVIL.py
@@ -61,7 +61,7 @@ async def amireallyalive(alive):
     reply_to_id = await reply_id(alive)
 
     if HELL_IMG:
-        hell_caption = f"**{🔥 𝐃𝟑𝐕𝐈𝐋 BOT 🔥}**\n\n"
+        hell_caption = f"**{𝐃𝟑𝐕𝐈𝐋 BOT}**\n\n"
         hell_caption += f"≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\n"
         hell_caption += f"__**BOT STATUS**__\n\n"
         hell_caption += f"**★ Telethon version :** `1.15.0`\n"
