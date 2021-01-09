593317385c176bdedc48e4305cb5f760852f28ca
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-01-09 09:55:39 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -17,9 +17,9 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "please check out to an official branch, and re-start the updater."
 )
 OFFICIAL_UPSTREAM_REPO = "https://github.com/sameerpanthi/SAVAGE"
-BOT_IS_UP_TO_DATE = "❂savage BOT is up-to-date!❂"
+BOT_IS_UP_TO_DATE = "❂𝐃𝟑𝐕𝐈𝐋 BOT is up-to-date!❂"
 NEW_BOT_UP_DATE_FOUND = (
-    "**SAVAGE BOT  Update Found For** {branch_name}\n"
+    "**𝐃𝟑𝐕𝐈𝐋 BOT  Update Found For** {branch_name}\n"
     "\n\n{changelog}\n"
     "Pulling Updates !!"
 )
@@ -73,7 +73,7 @@ async def updater(message):
     )
 
     if not changelog:
-        await message.edit("**﹁﹁Updating savage BOT** \n**Version** : `2.0` \n**Telethon** : `1.15.0` \n**Status** : `«Pulling Updates»` \n『**Thank You For Using savage BOT !**』")
+        await message.edit("**﹁﹁Updating 𝐃𝟑𝐕𝐈𝐋 BOT** \n**Version** : `2.0` \n**Telethon** : `1.15.0` \n**Status** : `«Pulling Updates»` \n『**Thank You For Using 𝐃𝟑𝐕𝐈𝐋 BOT !**』")
         await asyncio.sleep(5)
  
     message_one = NEW_BOT_UP_DATE_FOUND.format(
