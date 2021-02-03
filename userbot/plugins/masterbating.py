32f6cdc4cf11aa70b219e5748783e8e09d90e82f
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-02-03 11:34:56 +0530

    Create masterbating.py

---
+++ userbot/plugins/masterbating.py
@@ -0,0 +1,60 @@
+"""
+Available Commands
+.masterbate"""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"masterbate.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.3
+
+    animation_ttl = range(0, 100)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "masterbate":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "8✊️===D",
+
+            "8=✊️==D",
+
+            "8==✊️=D",
+
+            "8===✊️D",
+
+            "8==✊️=D",
+
+            "8=✊️==D",
+
+            "8✊️===D",
+
+            "8===✊️D💦",
+
+            "8==✊️=D💦💦",
+
+            "8=✊️==D💦💦💦"
+            "8===✊D🌊🌊🌊🌊🌊🌊🌊🌊🌊🌊" 
+            "I am sad yrr ye krna pda 🥺😂"       
+        ]
+
+        for i in animation_ttl:
+        
+            await asyncio.sleep(animation_interval)
+        
+            await event.edit(animation_chars[i % 8])
