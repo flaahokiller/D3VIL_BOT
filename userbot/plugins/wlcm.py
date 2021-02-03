611e278a74c60f830b4ae6a46af20387b503afcb
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-02-03 10:34:31 +0530

    Create wlcm.py

---
+++ userbot/plugins/wlcm.py
@@ -0,0 +1,46 @@
+ 
+from userbot.utils import admin_cmd
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+@borg.on(admin_cmd(pattern="wlcm$"))
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+async def gn(event):
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    await event.edit("╔╦╦╦═╦╗╔═╦═╦══╦═╗ ║║║║╩╣╚╣═╣║║║║║╩╣ ╚══╩═╩═╩═╩═╩╩╩╩═╝")
