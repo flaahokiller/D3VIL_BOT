c9ea28b1f10e6b5f992c825a95255132b8220ae8
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-02-03 10:52:49 +0530

    Update wlcm.py

--- userbot/plugins/wlcm.py
+++ userbot/plugins/wlcm.py
@@ -11,10 +11,3 @@ async def gn(event):
 
 
  
-@borg.on(admin_cmd(pattern="wlcm2$")) 
- 
-async def gn(event):
- 
-    await event.edit("We are made for goodness.❤️ We are made for love.❤️ We are made for friendliness.❤️ We are made for togetherness.❤️ We are made for all of the beautiful things that you and I know.❤️ We are made to tell the world that there are no outsiders.✅ All are welcome: black, white, red, yellow, rich, poor, educated, not educated, male, female, gay, straight, all, all, all. We all belong to this family, this human family, God's family🥺🙏.")
-
-
